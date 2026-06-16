ca543ff740cb6c0f1148363e1eb6bb22ef62eb08
manolitobes <manolitobes@gmail.com>
2026-06-15 21:10:45 -0600

    Creacion de nuevos componentes para una tarjeta

---
+++ src/components/PostCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post-card">
+    <h3>Presentacion</h3>
+    <p>
+      Ingeniero en sistemas computacionales con especialidad en bases de datos
+    </p>
+    <PostBotton/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+    import PostBotton from "./PostBotton.vue";
+</script>
+
+<style scoped>
+.post-card{
+    background-color: black;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid white;
+}
+
+.post-card p{
+    font-style: italic;
+    color: white;
+}
+</style>
